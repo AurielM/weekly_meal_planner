9f0a62a042e06728254fdc284c71858c4f733878
amcshaw <auriel.mcshaw@oxehealth.com>
2023-01-23 15:27:43 +0000

    meal plan meals output first

--- meal_generator.py
+++ meal_generator.py
@@ -10,6 +10,7 @@ meal_plan = set()
 while len(meal_plan) < 2:
     key, value = random.choice(list(meals_vs_ingredients.items()))
     meal_plan.add(key)
+print(f'\nThis week\'s meal plan {list(meal_plan)}')
 for meal in meal_plan:
     print(f"\nMeal: {meal}")
     required_ingredients = meals_vs_ingredients[meal]
