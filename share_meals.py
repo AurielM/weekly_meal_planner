4fd38f11188c4e22dbe90c92db8d1c5228a84593
amcshaw <auriel.mcshaw@oxehealth.com>
2023-02-03 15:47:21 +0000

    converted original script into a class for the meal_generator and re-structured into methods within said class

---
+++ share_meals.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+import sys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+class MealShare():
+    
+
+    def __init__(self):
+        pass
