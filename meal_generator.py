9b6887e8462ac836ae159ad4179090cf345ae11b
amcshaw <auriel.mcshaw@oxehealth.com>
2023-02-14 06:51:20 +0000

    re-formatted for ease of reading

--- meal_generator.py
+++ meal_generator.py
@@ -2,15 +2,12 @@ from meals import meals_vs_ingredients
 import random
 
 
-class MealGenerator():
-
-
+class MealGenerator:
     def __init__(self):
         self.single_meal = 0
         self.shopping_list = []
         self.meal_plan = set()
         self.required_ingredients = []
-
 
     def meal_and_ingredients(self, meal_plan):
         for meal in meal_plan:
@@ -19,13 +16,11 @@ class MealGenerator():
             self.shopping_list.extend(self.required_ingredients)
         return self.shopping_list
 
-
     def create_meal_plan(self):
         while len(self.meal_plan) < 2:
             key = random.choice(list(meals_vs_ingredients.keys()))
             self.meal_plan.add(key)
         return self.meal_plan
-
 
     def final_shopping_list(self, shopping_list):
         formatted_shopping_list = ""
@@ -33,9 +28,8 @@ class MealGenerator():
             if item in formatted_shopping_list:
                 pass
             else:
-                formatted_shopping_list += item + '\n'
+                formatted_shopping_list += item + "\n"
         return formatted_shopping_list
-
 
     def generate_meal(self):
         return self.final_shopping_list(self.meal_and_ingredients(self.create_meal_plan()))
